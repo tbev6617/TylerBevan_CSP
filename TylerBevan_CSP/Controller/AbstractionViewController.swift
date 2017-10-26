5f5ff5015ada3c2826ad864fba825da950db5307
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2017-10-26 08:09:36 -0600

    Made a class for each of the 7 big ideas

---
+++ TylerBevan_CSP/Controller/AbstractionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AbstractionViewController.swift
+//  TylerBevan_CSP
+//
+//  Created by Bevan, Tyler on 10/26/17.
+//  Copyright © 2017 Bevan, Tyler. All rights reserved.
+//
+
+import UIKit
+
+class AbstractionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
