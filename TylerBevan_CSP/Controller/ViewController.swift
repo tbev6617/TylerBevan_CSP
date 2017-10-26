053e97848afcd1975075130661661327a26e8e30
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2017-10-26 07:58:23 -0600

    Created a CSP project

---
+++ TylerBevan_CSP/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TylerBevan_CSP
+//
+//  Created by Bevan, Tyler on 10/26/17.
+//  Copyright © 2017 Bevan, Tyler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
