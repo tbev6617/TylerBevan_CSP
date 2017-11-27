65296f5bd261d6ccbfe26db8033a877b38ff5a7f
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2017-11-27 08:41:01 -0700

    Made a method to swipe left or right

--- TylerBevan_CSP/Controller/AbstractionViewController.swift
+++ TylerBevan_CSP/Controller/AbstractionViewController.swift
@@ -11,39 +11,89 @@ import UIKit
 public class AbstractionViewController: UIPageViewController, UIPageViewControllerDataSource
 {
     //MARK: Array of subviews
-    private (set) lazy var orderedAbractionViews : [UIViewController] =
+    private (set) lazy var orderedAbstractionViews : [UIViewController] =
     {
         return
         [
-            self.newAbstractnViewController(abstractionLevel: "Block"),
-            self.newAbstractnViewController(abstractionLevel: "Java"),
-            self.newAbstractnViewController(abstractionLevel: "ByteCode"),
-            self.newAbstractnViewController(abstractionLevel: "Binary"),
-            self.newAbstractnViewController(abstractionLevel: "AndGate"),
+            self.newAbstractionViewController(abstractionLevel: "Block"),
+            self.newAbstractionViewController(abstractionLevel: "Java"),
+            self.newAbstractionViewController(abstractionLevel: "ByteCode"),
+            self.newAbstractionViewController(abstractionLevel: "Binary"),
+            self.newAbstractionViewController(abstractionLevel: "AndGate"),
         ]
     } ()
-    override func viewDidLoad()
+    //helper method to retrieve the correct ViewController
+    private func newAbstractionViewController(abstractionLevel : String) -> UIViewController
     {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning()
-    {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\(abstractionLevel)ViewController")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override public func viewDidLoad()
+    {
+        super.viewDidLoad()
+        dataSource = self;
+        
+        if let firstViewController = orderedAbstractionViews.first
+        {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
     }
-    */
-
+    //MARK:- Required protocol methods for UIPageViewControllerDataSource
+    //swipe left method
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore
+        viewController: UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+        else
+        {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0
+        else
+        {
+            return orderedAbstractionViews.last
+        }
+        
+        guard orderedAbstractionViews.count > previousIndex
+        else
+        {
+            return nil
+        }
+        return orderedAbstractionViews[previousIndex]
+    }
+    //swipe right method
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter
+        viewController: UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+        else
+        {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard nextIndex >= 0
+        else
+        {
+            return nil
+        }
+        
+        guard nextIndex < orderedAbstractionViews.count
+        else
+        {
+            return orderedAbstractionViews.first
+        }
+        return orderedAbstractionViews[nextIndex]
+    }
+    override public func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
 }
