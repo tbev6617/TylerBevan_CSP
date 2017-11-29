25b418cd2dfbce2d558c61ad52afb6b788ded714
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2017-11-29 08:17:45 -0700

    Made two internet view controllers

---
+++ TylerBevan_CSP/Controller/InternetDetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  InternetDetailViewController.swift
+//  TylerBevan_CSP
+//
+//  Created by Bevan, Tyler on 11/29/17.
+//  Copyright © 2017 Bevan, Tyler. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+public class InternetDetailViewController: UIViewController
+{
+    //MARK: Add GUI controls
+    
+    override public func viewDidLoad()
+    {
+        super.viewDidLoad()
+        setup()
+    }
+    private func setup() -> Void
+    {
+        
+    }
+}
---
+++ TylerBevan_CSP/Controller/InternetMasterViewController.swift
@@ -0,0 +1,61 @@
+//
+//  InternetMasterViewController.swift
+//  TylerBevan_CSP
+//
+//  Created by Bevan, Tyler on 10/26/17.
+//  Copyright © 2017 Bevan, Tyler. All rights reserved.
+//
+
+import UIKit
+
+class InternetMasterViewController: UITableViewController
+{
+    private (set) lazy var internetTopics : [String] =
+    {
+        return [
+            "Definitions",
+            "CSP",
+            "CTEC",
+            "Canyons",
+            "Twitter",
+            "Swift Guide"
+        ]
+    }()
+    
+    private var detailViewController : InternetDetailViewController?
+    
+    private func setup() -> Void
+    {
+        
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        setup()
+        self.clearsSelectionOnViewWillAppear = false
+
+    }
+    // Mark: - Table view data source
+    
+    override public func numberOfSections(in tableView: UITableView) -> Int
+    {
+        // #warning
+        return 1;
+    }
+    
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return internetTopics.count
+    }
+    
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let currentText = internetTopics[indexPath.row]
+        cell.textLabel!.text = currentText
+        
+        return cell
+    }
+
+}
