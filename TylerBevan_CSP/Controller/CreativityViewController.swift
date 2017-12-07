79f908a4f7a2f0cc731a1dff1098f064246ba63c
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2017-12-07 08:29:34 -0700

    made UICollectionView methods

--- TylerBevan_CSP/Controller/CreativityViewController.swift
+++ TylerBevan_CSP/Controller/CreativityViewController.swift
@@ -8,31 +8,79 @@
 
 import UIKit
 
-class CreativityViewController: UIViewController
+public class CreativityViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout
 {
-
-    override func viewDidLoad()
+    private let reuseIdentifier = "artIdentifier"
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    private let itemsPerRow : CGFloat = 3
+    
+    private lazy var artSelection : [UIImage?] =
+    {
+        return [
+            UIImage(named: "haiku"),
+            UIImage(named: "penguinTree")
+        ]
+    }()
+    
+    var largePhotoIndexPath: IndexPath?
+    {
+        didSet
+        {
+            var indexPaths = [IndexPath]()
+            if let largePhotoIndexPath = largePhotoIndexPath
+            {
+                indexPaths.append(largePhotoIndexPath)
+            }
+            if let oldValue = oldValue
+            {
+                indexPaths.append(oldValue)
+            }
+            collectionView?.performBatchUpdates(
+                {
+                    self.collectionView?.reloadItems(at: indexPaths)
+                })
+            {
+                completed in
+                
+                if let largePhotoIndexPath = self.largePhotoIndexPath
+                {
+                    self.collectionView?.scrollToItem(at: largePhotoIndexPath,
+                                                      at: .centeredVertically,
+                                                      animated: true)
+                }
+            }
+        }
+    }
+    override public func viewDidLoad()
     {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning()
+    override public func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    //MARK:- UICollectionView methods
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override public func numberOfSections(in collectionView: UICollectionView) -> Int
+    {
+        return 1
     }
-    */
-
+    override public func collectionView(_ collectionView: UICollectionView,
+                                        numberOfItemsInSection section: Int) -> Int
+    {
+        return artSelection.count
+    }
+    override public func collectionView(_ collectionView: UICollectionView,
+                                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let artCell = collectionView.dequeueReusableCell(withReuseItifier: reuseIdentifier, for: indexPath) as! ArtCell
+        
+        artCell.backgroundColor = .purple
+        artCell.imageView.image = artSelection[indexPath.row]
+        artCell.imageName.text = "My Art"
+        
+        return artCell
+    }
 }
