ef0bb5e9c51f3babbbd35847dcaa4522c38e0edc
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2018-01-09 09:55:38 -0700

    did everything until step 4C

--- TylerBevan_CSP/Controller/DataView/DataViewController.swift
+++ TylerBevan_CSP/Controller/DataView/DataViewController.swift
@@ -11,28 +11,107 @@ import UIKit
 class DataViewController: UITableViewController
 {
 
+    @IBOutlet weak var bucketItemSymbol: UILabel!
+    @IBOutlet weak var bucketItemText: UILabel!
+    @IBOutlet weak var bucketItemSignature: UILabel!
+    
+    var bucketItem : BucketItem!
+    {
+        didSet
+        {
+            updateCellView()
+        }
+    }
+    lazy var bucketList : [BucketItem] =
+    {
+        return loadBucketListFromFile()
+    } ()
+    
+    private func loadBucketListFromFile() -> [BucketItem]
+    {
+        var items = [BucketItem]()
+        if let filePath = Bundle.main.url(forResource: "bucket", withExtension: "csv")
+        {
+            do
+            {
+                let input = try String(contentsOf: filePath)
+                let bucketLines = input.components(separatedBy: "\n")
+                for line in bucketLines
+                {
+                    let item = line.components(separatedBy: ",")
+                    items.append(BucketItem(contents: item[0], author: item[1]))
+                }
+            }
+            catch
+            {
+                print("File load error")
+            }
+        }
+        return items;
+    }
+    
+    //MARK: TableView code
+    
+    override public func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return bucketList.count
+    }
+    
+    //TODO step 4C override public func tableView()
+    
+    private func randomEmoji() -> String
+    {
+        let emojiStart = 0x1F601
+        let emojiEnd = 0x1F64F
+        let symbolStart = 0x1F680
+        let symbolEnd = 0x1F6C5
+        
+        let emojiRange = 79
+        let symbolRange = 70
+        
+        let ascii = emojiStart + Int(arc4random_uniform(UIInt32(emojiRange)))
+        let emoji = UnicodeScalar(ascii)?.description
+        return emoji
+    }
+    
+    private func updateCellView() -> Void
+    {
+        if (bucketItem != nil)
+        {
+            bucketItemSignature.text = bucketItem.itemAuthor
+            bucketItemText.text = bucketItem.itemContents
+        }
+        else
+        {
+            bucketItemSignature.text = "author goes here"
+            bucketItemText.text = "text goes here"
+        }
+        bucketItemSymbol.text = randomEmojiSymbol()
+    }
+    
+    public override func awakeFromNib()
+    {
+        super.awakeFromNib()
+    }
+    
+    public override func setSelected(_ selected : Bool, animated : Bool)
+    {
+        super.setSelected(selected, animated: animated)
+    }
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
